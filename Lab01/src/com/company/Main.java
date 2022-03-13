bf9e42622b9408cfd62d6415d78f2533af127739
Łukasz Kot <lukaszkot03@gmail.com>
2022-03-13 15:12:08 +0100

    Done 3 task

--- Lab01/src/com/company/Main.java
+++ Lab01/src/com/company/Main.java
@@ -3,6 +3,27 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int[] array = {4, 5, 7, 11, 12, 15, 15, 21, 40, 45};
+        System.out.println(searchIndex(array, 11));
+    }
+
+    public static int searchIndex(int[] array, int value)
+    {
+        int index = 0;
+        var limit = array.length - 1;
+        while (index <= limit) {
+            double point = Math.ceil((double)(index + limit) / (double)2);
+            var entry = array[(int)point];
+            if (value > entry) {
+                index = (int)point + 1;
+                continue;
+            }
+            if (value < entry) {
+                limit = (int)point - 1;
+                continue;
+            }
+            return (int)point;
+        }
+        return -1;
     }
 }
