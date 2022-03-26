469a0e8d71ba30a2aa688873ef6dce7f068e74de
Łukasz Kot <lukaszkot03@gmail.com>
2022-03-26 14:00:08 +0100

    Added missing TestObject class.

---
+++ Lab01/src/com/company/TestObject.java
@@ -0,0 +1,29 @@
+package com.company;
+
+public class TestObject {
+    String name;
+    int value;
+
+    public TestObject(){}
+
+    public TestObject(String name, int value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
