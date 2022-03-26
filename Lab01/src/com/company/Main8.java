d67ca8a5ed714609ec24fa496e29ae2479ea7b6d
Łukasz Kot <lukaszkot03@gmail.com>
2022-03-26 13:59:14 +0100

    Implemented task 8

--- Lab01/src/com/company/Main8.java
+++ Lab01/src/com/company/Main8.java
@@ -2,18 +2,23 @@ package com.company;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-public class Main7 {
+public class Main8 {
     public static void main(String[] args) {
         ObjectMapper objectMapper = new ObjectMapper();
 
-        TestObject userObject = new TestObject("Test", 21);
-        String testObjectAsJson = null;
+        String str = "{\"name\":\"Test\",\"value\":21}";
+        TestObject obj = null;
         try {
-            testObjectAsJson = objectMapper.writeValueAsString(userObject);
+            obj = objectMapper.readValue(str, TestObject.class);
         } catch (JsonProcessingException e) {
             e.printStackTrace();
         }
 
-        System.out.println(testObjectAsJson);
+        if(obj != null)
+        {
+            System.out.println(obj.getName());
+            System.out.println(obj.getValue());
+        }
+
     }
 }
